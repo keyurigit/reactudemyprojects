c3937b2fb3bee2688beba7e1a9fded86b22fff57
keyuri <kejp581994@gmail.com>
2025-04-02 15:55:40 +0530

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import Person from "./Person";
+
+const List = ({people}) =>{
+    return (
+        people.map((person)=>{
+            return <Person key={person.id} {...person} />;
+        })
+    )
+}
+
+export default List
