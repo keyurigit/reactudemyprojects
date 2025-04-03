241c838463cb421a339f01cd2c819ba3428f64cd
keyuri <kejp581994@gmail.com>
2025-04-03 15:38:42 +0530

    Tours useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,86 @@
-import { useState } from "react"
-import data from "./data"
-import List from "./List";
+// 1. Birthday Buddy
+
+
+// import { useState } from "react"
+// import data from "./data"
+// import List from "./List";
+// function App() {
+
+//   const [people,setPeople] = useState(data);
+
+//   return (
+//     <main>
+//       <section className="container">
+//         <h3>{people.length} Birthdays Today</h3>
+//         <List people={people}/>
+//         <button onClick={()=>setPeople([])} type='button'
+//           className='btn btn-block'>clear all</button>
+//       </section>
+//     </main>
+//   )
+// }
+
+// export default App
+
+// 2. Tours
+
+import { useState,useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = 'https://www.course-api.com/react-tours-project';
+
 function App() {
 
-  const [people,setPeople] = useState(data);
+  const [isLoading,setIsLoading] = useState(true);
+  const [tours,setTours] = useState([]);
 
+  const fetchtours = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setIsLoading(false);
+      setTours(data);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(() => {
+    fetchtours();;
+  }, []);  
+
+  const removeTour = (id) =>{
+    const newTours = tours.filter((tour)=>{
+      return(tour.id !== id )
+    });
+    setTours(newTours);
+  }
+  
+  if(isLoading){
+    return (
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchtours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <section className="container">
-        <h3>{people.length} Birthdays Today</h3>
-        <List people={people}/>
-        <button onClick={()=>setPeople([])} type='button'
-          className='btn btn-block'>clear all</button>
-      </section>
+      <Tours tours = {tours} removeTour={removeTour}/>
     </main>
   )
 }
---
+++ src/Loading.jsx
@@ -0,0 +1,8 @@
+const Loading = () =>{
+    // return(
+    //     <h2>Loading</h2>
+    // )
+    return <div className='loading'></div>;
+}
+
+export default Loading
